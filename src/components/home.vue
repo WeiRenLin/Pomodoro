8e152010c893ff242b79e3317ff6a2c563787d3e
William Lin <1609005@iisigroup.com>
2019-07-19 10:04:14 +0800

    部屬專案 home及list頁面修改

--- src/components/home.vue
+++ src/components/home.vue
@@ -17,32 +17,47 @@
       </div>
       <div class="home__left__newMission__countdown">{{minTime+":"+secTime}}</div>
       <div class="home__left__newMission__list">
-        <div  class="home__left__newMission__list__task">
+        <div class="home__left__newMission__list__task">
           <ul>
             <li v-for="item in list" :key="item.id">
-               <div class="home__left__newMission__list__task__small__dot"></div> 
-               {{item.name}}
-               <div class="home__left__newMission__list__task__small__triangle" @click="changeTask(item)">
-              <i class="far fa-play-circle changeTask"></i>
-               </div>
-               </li>
+              <div class="home__left__newMission__list__task__small__dot"></div>
+              {{item.name}}
+              <div
+                class="home__left__newMission__list__task__small__triangle"
+                @click="changeTask(item)"
+              >
+                <i class="far fa-play-circle changeTask"></i>
+              </div>
+            </li>
           </ul>
-         </div>
+        </div>
       </div>
     </div>
+    <!-- <svg x="0px" y="0px" width="200px" height="200px" viewBox="0 0 200 200">
+      <circle
+        class="path"
+        stroke="hotpink"
+        fill="none"
+        stroke-width="5"
+        stroke-dasharray="1000"
+        cx="100"
+        cy="100"
+        r="80"
+      />
+    </svg> -->
     <div class="home__right">
       <div class="home__right__bar">
         <router-link to="/list">
-        <i class="fas fa-bars"></i>
+          <i class="fas fa-bars"></i>
         </router-link>
         <router-link to="/">
-        <i class="fas fa-chart-bar"></i>
+          <i class="fas fa-chart-bar"></i>
         </router-link>
         <router-link to="/">
-        <i class="fas fa-podcast"></i>
+          <i class="fas fa-podcast"></i>
         </router-link>
         <div class="home__right__bar__text">
-        <span>POMODORO</span>
+          <span>POMODORO</span>
         </div>
       </div>
       <div class="home__right__dot">
@@ -72,11 +87,13 @@ export default {
       time: null,
       minTime: null,
       secTime: null,
-      list:[
-        {id:1,name:"the second thing to do today"},{id:2,name:"the third thing to do today"},{id:3,name:"the forth thing to do today"}
+      list: [
+        { id: 1, name: "the second thing to do today" },
+        { id: 2, name: "the third thing to do today" },
+        { id: 3, name: "the forth thing to do today" }
       ],
-      title:'the First thing to do today',
-    }
+      title: "the First thing to do today"
+    };
   },
   props: {},
   methods: {
@@ -84,12 +101,12 @@ export default {
       var vm = this;
       vm.isPlay = !vm.isPlay;
     },
-    changeTask:function(item){
+    changeTask: function(item) {
       var vm = this;
       vm.title = item.name;
-      vm.time = 25*60;
-       vm.minTime = Math.floor(vm.time / 60);
-        vm.secTime = vm.time % 60 > 10 ? vm.time % 60 : "0" + (vm.time % 60);
+      vm.time = 25 * 60;
+      vm.minTime = Math.floor(vm.time / 60);
+      vm.secTime = vm.time % 60 > 10 ? vm.time % 60 : "0" + (vm.time % 60);
     }
   },
   watch: {
@@ -100,21 +117,20 @@ export default {
           clearInterval(timer);
           return;
         }
-         if(vm.time <0){
-        vm.minTime =0;
-        vm.secTime=0;
-        return;
-      }
+        if (vm.time < 0) {
+          vm.minTime = 0;
+          vm.secTime = 0;
+          return;
+        }
         vm.time = vm.time - 1;
         vm.minTime = Math.floor(vm.time / 60);
         vm.secTime = vm.time % 60 > 10 ? vm.time % 60 : "0" + (vm.time % 60);
       }, 1000);
-     
     }
   },
   mounted() {
     var vm = this;
-    vm.time = 25*60;
+    vm.time = 25 * 60;
     vm.minTime = vm.time / 60;
     vm.secTime = vm.time % 60 > 9 ? vm.time % 60 : "0" + (vm.time % 60);
   }
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="list">
-        <div class="list__bar">
-        <router-link to="/list" :class="isPage === true? 'hrefActive':''">
+  <div class="list">
+    <div class="list__bar">
+      <router-link to="/list" :class="isPage === true? 'hrefActive':''">
         <i class="fas fa-bars"></i>
         <span>To-do list</span>
-        </router-link>
-        <router-link to="/">
+      </router-link>
+      <router-link to="/">
         <i class="fas fa-chart-bar"></i>
         <span>Analytics</span>
-        </router-link>
-        <router-link to="/">
+      </router-link>
+      <router-link to="/">
         <i class="fas fa-podcast"></i>
         <span>Ringtones</span>
-        </router-link>
-        <!-- <div class="home__right__bar__text">
-        <span>POMODORO</span>
-        </div> -->
-      </div>
+      </router-link>
     </div>
+    <label class="list__newMission">
+      <input type="text" class="list__newMission__new__task" placeholder="ADD A NEW MISSION..." />
+    </label>
+  </div>
 </template>
 <script>
 export default {
-    name: "list",
+  name: "list",
   data() {
-      return{
-      isPage:true,
-      }
+    return {
+      isPage: true
+    };
   }
-}
+};
 </script>
 
